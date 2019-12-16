c47721f5c4d46d5080792eca26ea8f9dae3846b5
Manuel Pisciotta <m.pisciotta8@studenti.unisa.it>
2019-12-16 17:03:02 +0100

    Modifiche RAD/SDD, Aggiunta ODD

--- Desktop/TutoratoSmart/Database/Creazione Database TutoratoSmart.sql
+++ Desktop/TutoratoSmart/Database/Creazione Database TutoratoSmart.sql
@@ -3,72 +3,107 @@ CREATE SCHEMA TutoratoSmart;
 
 USE TutoratoSmart;
 
-CREATE TABLE Utente
-(	Email				VARCHAR(45)		NOT NULL,
-	Ruolo				ENUM('Studente', 'Tutor', 'Responsabile Commissione')		NOT NULL	DEFAULT 'Studente',
-    Nome				VARCHAR(30)		NOT NULL,
-    Cognome				VARCHAR(30)		NOT NULL,
-    Sesso				ENUM('M', 'F')	NOT NULL,
+CREATE TABLE TS_User
+(	Email				VARCHAR(30)		NOT NULL,
     Pwd					VARCHAR(40)		NOT NULL,
-    Numero_Telefono		VARCHAR(15),
-    Matricola			CHAR(10)		NOT NULL,
+	FirstName			VARCHAR(30)		NOT NULL,
+    LastName			VARCHAR(30)		NOT NULL,
+    TelephoneNumber		VARCHAR(10)		NOT NULL,
+    Sex					ENUM('M', 'F')  NOT NULL,
+    RegistrationNumber	CHAR(10)		NOT NULL,
 PRIMARY KEY (Email),
-UNIQUE (Matricola));
+UNIQUE (RegistrationNumber));
 
+CREATE TABLE Student
+(	Email				VARCHAR(30)		NOT NULL,
+	AcademicYear		INT				NOT NULL,
+PRIMARY KEY (Email),
+FOREIGN KEY (Email) REFERENCES TS_User(Email));
 
-CREATE TABLE Registro
-(	ID_Registro			INT				NOT NULL	AUTO_INCREMENT,
-	Stato				ENUM('Da Compilare', 'Compilato', 'Approvato')		NOT NULL 	DEFAULT 'Da Compilare',
-    Percentuale_Completamento	FLOAT	NOT NULL	DEFAULT 0,
-    Responsabile		VARCHAR(45)		NOT NULL,
-PRIMARY KEY (ID_Registro),
-FOREIGN KEY (Responsabile) REFERENCES Utente(Email));
+CREATE TABLE Tutoring_Commission_Member
+(	Email				VARCHAR(30)		NOT NULL,
+PRIMARY KEY (Email),
+FOREIGN KEY (Email) REFERENCES TS_User(Email));
 
+CREATE TABLE Register
+(	IdRegister			INT				NOT NULL	AUTO_INCREMENT,
+	State				ENUM('Non approvato', 'Approvato')		NOT NULL 	DEFAULT 'Non approvato',
+    ValidatedHours		INT				NOT NULL	DEFAULT 0,
+    TotalHours			INT				NOT NULL,
+    PercentageComplete	FLOAT			NOT NULL	DEFAULT 0,
+PRIMARY KEY (IdRegister));
 
 CREATE TABLE Tutor
-(	Email				VARCHAR(45)		NOT NULL,
-	Data_Inizio			DATE			NOT NULL,
-    Data_Termine		DATE			NOT NULL,
-    ID_Registro			INT				NOT NULL,
+(	Email				VARCHAR(30)		NOT NULL,
+	StartDate			DATE			NOT NULL,
+    CommissionMember	VARCHAR(30)		NOT NULL,
+    RegisterId			INT				NOT NULL,
 PRIMARY KEY (Email),
-FOREIGN KEY (Email) REFERENCES Utente(Email) ON DELETE CASCADE,
-FOREIGN KEY (ID_Registro) REFERENCES Registro(ID_Registro));
+FOREIGN KEY (Email) REFERENCES TS_User(Email),
+FOREIGN KEY (CommissionMember) REFERENCES Tutoring_Commission_Member(Email),
+FOREIGN KEY (RegisterId) REFERENCES Register(IdRegister));
 
+CREATE TABLE Request
+(	IdRequest			INT				NOT NULL	AUTO_INCREMENT,
+	State				ENUM('In valutazione', 'Accettata')  NOT NULL	DEFAULT 'In valutazione',
+	StudentComment		VARCHAR(240)	NOT NULL,
+    RequestDate			DATE			NOT NULL,
+    RequestTime			INT				NOT NULL,
+    Duration			INT				NOT NULL,
+    Student				VARCHAR(30)		NOT NULL,
+PRIMARY KEY (IdRequest),
+FOREIGN KEY (Student) REFERENCES Student(Email));
 
-CREATE TABLE Prenotazione
-(	ID_Prenotazione		INT				NOT NULL	AUTO_INCREMENT,
-	Giorno				DATE			NOT NULL,
-    Ora					CHAR(5)			NOT NULL,
-    Studente			VARCHAR(45)		NOT NULL,
-PRIMARY KEY (ID_Prenotazione),
-FOREIGN KEY (Studente) REFERENCES Utente(Email));
+CREATE TABLE Appointment
+(	IdAppointment		INT				NOT NULL	AUTO_INCREMENT,
+	Details				VARCHAR(240)	NOT NULL,
+    RequestId			INT				NOT NULL,
+    Tutor				VARCHAR(30)		NOT NULL,
+PRIMARY KEY (IdAppointment),
+FOREIGN KEY (RequestId) REFERENCES Request(IdRequest),
+FOREIGN KEY (Tutor) REFERENCES Tutor(Email));
 
-   
-CREATE TABLE Ora_Lavorativa
-(	Tutor 				VARCHAR(45)		NOT NULL,
-	Giorno				DATE			NOT NULL,
-    Ora					CHAR(5)			NOT NULL,
-    Tipologia			ENUM('Sportello informativo', 'Assistenza Esame', 'Organizzazione Seminario', 'Seminario', 'Organizzazione Evento', 'Evento')		NOT NULL,
-    Descrizione			VARCHAR(3000)	NOT NULL,
-    Numero_Registro		INT				NOT NULL,
-PRIMARY KEY (Tutor, Giorno, Ora),
-FOREIGN KEY (Tutor) REFERENCES Utente(Email),
-FOREIGN KEY (Numero_Registro) REFERENCES Registro(ID_Registro));
+CREATE TABLE Activity_Tutor
+(	IdActivity			INT				NOT NULL	AUTO_INCREMENT,
+	Category			ENUM('Sportello informativo', 'Assistenza Esame', 'Organizzazione Seminario', 'Seminario', 'Organizzazione Evento', 'Evento')	NOT NULL,
+	ActivityDate		DATE			NOT NULL,
+    StartTime			INT				NOT NULL,
+    FinishTime			INT				NOT NULL,
+    Hours				FLOAT			NOT NULL,
+    State				ENUM('In valutazione', 'Convalidata')	NOT NULL	DEFAULT 'In valutazione',
+    Details				VARCHAR(320)	NOT NULL,
+    Tutor				VARCHAR(30)		NOT NULL,
+    RegisterId			INT				NOT NULL,
+PRIMARY KEY (IdActivity),
+FOREIGN KEY (Tutor) REFERENCES TS_User(Email),
+FOREIGN KEY (RegisterId) REFERENCES Register(IdRegister));
 
+CREATE TABLE Work_Day
+(	WorkDayName			ENUM('Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì')	NOT NULL,
+	StartTime			INT				NOT NULL,
+    FinishTime			INT				NOT NULL,
+    IsOpen				BOOLEAN			NOT NULL 	DEFAULT FALSE,
+    CommissionMember	VARCHAR(30)		NOT NULL,
+PRIMARY KEY (WorkDayName),
+FOREIGN KEY (CommissionMember) REFERENCES Tutoring_Commission_Member(Email));
 
-CREATE TABLE Gestisce
-(	Tutor 				VARCHAR(45)		NOT NULL,
-	Numero_Prenotazione	INT				NOT NULL,
-PRIMARY KEY (Tutor, Numero_Prenotazione),
-FOREIGN KEY (Tutor) REFERENCES Utente(Email),
-FOREIGN KEY (Numero_Prenotazione) REFERENCES Prenotazione(ID_Prenotazione));
+CREATE TABLE Manages
+(	Tutor 				VARCHAR(30)		NOT NULL,
+	RequestId			INT				NOT NULL,
+PRIMARY KEY (Tutor, RequestId),
+FOREIGN KEY (Tutor) REFERENCES Tutor(Email),
+FOREIGN KEY (RequestId) REFERENCES Request(IdRequest));
 
+CREATE TABLE Contained_In
+(	AppointmentId		INT				NOT NULL,
+	ActivityId			INT				NOT NULL,
+PRIMARY KEY (AppointmentId, ActivityId),
+FOREIGN KEY (AppointmentId) REFERENCES Appointment(IdAppointment),
+FOREIGN KEY (ActivityId) REFERENCES Activity_Tutor(IdActivity));
 
-CREATE TABLE Contenuta_In
-(	Numero_Prenotazione	INT				NOT NULL,
-	Tutor				VARCHAR(45)		NOT NULL,
-	Giorno				DATE			NOT NULL,
-    Ora					CHAR(5)			NOT NULL,
-PRIMARY KEY (Numero_Prenotazione, Tutor, Giorno, Ora),
-FOREIGN KEY (Numero_Prenotazione) REFERENCES Prenotazione(ID_Prenotazione),
-FOREIGN KEY (Tutor, Giorno, Ora) REFERENCES Ora_Lavorativa(Tutor, Giorno, Ora));
+CREATE TABLE Validates
+(	CommissionMember	VARCHAR(30)		NOT NULL,
+	ActivityId			INT				NOT NULL,
+PRIMARY KEY (CommissionMember, ActivityId),
+FOREIGN KEY (CommissionMember) REFERENCES Tutoring_Commission_Member(Email),
+FOREIGN KEY (ActivityId) REFERENCES Activity_Tutor(IdActivity));
